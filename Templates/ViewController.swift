a89032c473af7b36172181c0475aa41de39d404b
Vladimir Fibe <motiw@icloud.com>
2023-09-25 17:36:27 +0600

    Initial Commit

---
+++ Templates/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Templates
+//
+//  Created by Vladimir Fibe on 25.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
